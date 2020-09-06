62564da4451660c4b0d18a1240b66a95f86c41c7
Nikolas Paripovic <nikolas.paripovic@gmail.com>
2020-09-06 14:37:34 +0200

    Changed indentation

--- src/main/java/com/github/nikolasfunction/tournamentobserver/Main.java
+++ src/main/java/com/github/nikolasfunction/tournamentobserver/Main.java
@@ -3,8 +3,8 @@ package com.github.nikolasfunction.tournamentobserver;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
 }
