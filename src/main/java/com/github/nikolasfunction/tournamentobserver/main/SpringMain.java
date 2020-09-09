44eb500dd276d9d6960b47d4d1ba3c0d8b1f4906
Nikolas Paripovic <nikolas.paripovic@gmail.com>
2020-09-09 23:01:11 +0200

    Adjusted SpringMain

--- src/main/java/com/github/nikolasfunction/tournamentobserver/main/SpringMain.java
+++ src/main/java/com/github/nikolasfunction/tournamentobserver/main/SpringMain.java
@@ -1,57 +1,24 @@
 package com.github.nikolasfunction.tournamentobserver.main;
 
-import java.text.SimpleDateFormat;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.github.nikolasfunction.tournamentobserver.scanner.ITableInitializer;
-import com.github.nikolasfunction.tournamentobserver.web.IHtmlRequester;
-import com.github.nikolasfunction.tournamentobserver.web.clicktt.ITournamentTableUrlGenerator;
-import com.github.nikolasfunction.tournamentobserver.web.clicktt.parse.ITournamentTableParser;
 
 @Component
 public class SpringMain implements CommandLineRunner {
     
-    private final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
-    
-    private final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
-    
-    private final ITournamentTableParser tournamentTableParser;
-    
-    private final IHtmlRequester htmlRequester;
-    
-    private final ITournamentTableUrlGenerator tournamentTableUrlGenerator;
-    
     private final ITableInitializer tableInitializer;
     
     @Autowired
-    public SpringMain(ITournamentTableParser tournamentTableParser,
-            IHtmlRequester htmlRequester,
-            ITournamentTableUrlGenerator tournamentTableUrlGenerator,
-            ITableInitializer tableInitializer) {
-        this.tournamentTableParser = tournamentTableParser;
-        this.htmlRequester = htmlRequester;
-        this.tournamentTableUrlGenerator = tournamentTableUrlGenerator;
+    public SpringMain(ITableInitializer tableInitializer) {
         this.tableInitializer = tableInitializer;
     }
-
-
+    
     @Override
     public void run(String... args) throws Exception {
-        
         tableInitializer.initializeTable();
-        
-//      LocalDate date = LocalDate.now();        
-//      String url = tournamentTableUrlGenerator.generateTournamentTableUrl(date.getMonth(), Year.now());
-//      
-//      Document html = htmlRequester.requestHtml(url);
-//      ITournamentTable table = tournamentTableParser.parseTournamentTable(html);
-//      
-//      System.out.println(String.format("Found %d entries in %s %d:", table.getTournamentTableEntries().size(), table.getMonth().toString(), table.getYear().getValue()));
-//      table.getTournamentTableEntries().stream().forEach(entry -> System.out.println(String.format("%s um %s in %s", DATE_FORMAT.format(entry.getTime()), TIME_FORMAT.format(entry.getTime()), entry.getOrganizer())));
-      
     }
     
     
